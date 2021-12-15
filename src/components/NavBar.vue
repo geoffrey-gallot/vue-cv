15f193e70ede7a31416d76156a7d9bf081f02329
geoffrey-gallot <geoffrey.gallot@gmail.com>
2021-12-15 13:02:21 +0100

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top " id="navbar">
+      <a class="navbar-brand" tag="router-link" :to="{ path: '/' }">Accueil</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">A propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ma formation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mes projets</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style scoped>
+#navbar{
+    text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
